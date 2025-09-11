2ab0dbe946d2c35d7715eb23bcc8f8bbaa3a00b1
dimovdmitrii <dimovdima1509@gmail.com>
2025-09-11 22:28:25 +0200

    asgasegfsdg

--- src/components/mainPageCategories/index.jsx
+++ src/components/mainPageCategories/index.jsx
@@ -1,16 +1,15 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import { fetchCategories } from "../../redux/slices/categorySlice";
-import CategoryCard from "../cartCategories";
-import { API_URL } from "../../config/api";
-import styles from "./styles.module.css";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { fetchCategories } from '../../redux/slices/categorySlice';
+import CategoryCard from '../cartCategories';
+import styles from './styles.module.css';
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const SHOW_NAVIGATION_THRESHOLD = 4;
 const SWIPER_BREAKPOINTS = {
@@ -23,23 +22,18 @@ const SWIPER_BREAKPOINTS = {
 
 const MainPageCategories = () => {
   const dispatch = useDispatch();
-  const { categories, loading, error } = useSelector(
-    (state) => state.categories
-  );
+  const { categories, loading, error } = useSelector((state) => state.categories);
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
 
-  const showNavigation =
-    Array.isArray(categories) && categories.length > SHOW_NAVIGATION_THRESHOLD;
+  const showNavigation = categories.length > SHOW_NAVIGATION_THRESHOLD;
 
   const renderHeader = () => (
     <div className={styles.header}>
       <h2 className={styles.title}>Categories</h2>
-      <Link to="/categories" className={styles.allCategoriesBtn}>
-        All categories
-      </Link>
+      <Link to="/categories" className={styles.allCategoriesBtn}>All categories</Link>
     </div>
   );
 
@@ -47,30 +41,23 @@ const MainPageCategories = () => {
     modules: [Navigation, Pagination, Autoplay],
     spaceBetween: 32,
     slidesPerView: "auto",
-    navigation: showNavigation
-      ? { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }
-      : false,
-    pagination: showNavigation
-      ? { clickable: true, el: ".swiper-pagination" }
-      : false,
-    autoplay: showNavigation
-      ? { delay: 3000, disableOnInteraction: false }
-      : false,
+    navigation: showNavigation ? { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' } : false,
+    pagination: showNavigation ? { clickable: true, el: '.swiper-pagination' } : false,
+    autoplay: showNavigation ? { delay: 3000, disableOnInteraction: false } : false,
     loop: showNavigation,
     breakpoints: SWIPER_BREAKPOINTS,
     className: styles.swiper,
   });
 
-  const renderNavigation = () =>
-    showNavigation && (
-      <>
-        <div className={`swiper-button-prev ${styles.navButton}`}></div>
-        <div className={`swiper-button-next ${styles.navButton}`}></div>
-        <div className={`swiper-pagination ${styles.pagination}`}></div>
-      </>
-    );
+  const renderNavigation = () => showNavigation && (
+    <>
+      <div className={`swiper-button-prev ${styles.navButton}`}></div>
+      <div className={`swiper-button-next ${styles.navButton}`}></div>
+      <div className={`swiper-pagination ${styles.pagination}`}></div>
+    </>
+  );
 
-  if (loading)
+  if (loading) {
     return (
       <section className={styles.categoriesSection}>
         <div className={styles.container}>
@@ -79,23 +66,14 @@ const MainPageCategories = () => {
         </div>
       </section>
     );
+  }
 
-  if (error)
+  if (error) {
     return (
       <section className={styles.categoriesSection}>
         <div className={styles.container}>
           {renderHeader()}
           <div className={styles.error}>Error loading categories: {error}</div>
-        </div>
-      </section>
-    );
-
-  if (!Array.isArray(categories) || categories.length === 0) {
-    return (
-      <section className={styles.categoriesSection}>
-        <div className={styles.container}>
-          {renderHeader()}
-          <div className={styles.empty}>No categories found</div>
         </div>
       </section>
     );
@@ -109,12 +87,7 @@ const MainPageCategories = () => {
           <Swiper {...getSwiperConfig()}>
             {categories.map((category) => (
               <SwiperSlide key={category.id} className={styles.swiperSlide}>
-                <CategoryCard
-                  category={{
-                    ...category,
-                    image: `${API_URL}${category.image}`,
-                  }}
-                />
+                <CategoryCard category={category} />
               </SwiperSlide>
             ))}
           </Swiper>

d2075a36888629fb49af1476530857adfc330127
dimovdmitrii <dimovdima1509@gmail.com>
2025-09-11 09:24:39 +0200

    confif folder modifcation

--- src/config/api.js
+++ src/config/api.js
@@ -3,11 +3,11 @@ const getApiUrl = () => {
   // Если мы в продакшене (GitHub Pages), используем реальный API
   if (import.meta.env.PROD) {
     // Замените на ваш реальный API URL (полученный после деплоя)
-    return 'https://your-app-name.railway.app'; // или .onrender.com, или .herokuapp.com
+    return "https://petshop-project-blond.vercel.app/"; // или .onrender.com, или .herokuapp.com
   }
-  
+
   // В разработке используем локальный сервер
-  return 'http://localhost:3333';
+  return "http://localhost:3333";
 };
 
 export const API_URL = getApiUrl();

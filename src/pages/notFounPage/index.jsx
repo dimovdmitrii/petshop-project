5264c4906916766fec25d1ad1fb2d10eb3b6a9b8
dimovdmitrii <dimovdima1509@gmail.com>
2025-09-11 12:58:23 +0200

    404 page refactoring

--- src/pages/notFounPage/index.jsx
+++ src/pages/notFounPage/index.jsx
@@ -3,6 +3,12 @@ import { Typography, Button, Box } from "@mui/material";
 import img404 from "../../assets/images/404.png";
 import styles from "./styles.module.css";
 
+// Common styles
+const commonStyle = {
+  fontFamily: 'Montserrat, sans-serif',
+  textAlign: 'center'
+};
+
 function NotFounPage() {
   return (
     <div className={styles.container}>
@@ -13,11 +19,10 @@ function NotFounPage() {
           variant="h1" 
           className={styles.title}
           sx={{
-            fontFamily: 'Montserrat, sans-serif',
+            ...commonStyle,
             fontSize: { xs: "32px", sm: "48px", md: "64px" },
             fontWeight: 700,
             color: "#282828",
-            textAlign: "center",
             marginBottom: "20px",
             whiteSpace: "nowrap"
           }}
@@ -29,10 +34,9 @@ function NotFounPage() {
           variant="body1" 
           className={styles.description}
           sx={{
-            fontFamily: 'Montserrat, sans-serif',
+            ...commonStyle,
             fontSize: { xs: "16px", sm: "18px", md: "20px" },
             color: "#8B8B8B",
-            textAlign: "center",
             marginBottom: "40px",
             maxWidth: "664px",
             lineHeight: 1.6
@@ -53,26 +57,15 @@ function NotFounPage() {
             fontFamily: 'Montserrat, sans-serif',
             fontSize: "16px",
             fontWeight: 600,
-            padding: {
-              xs: "12px 24px",
-              lg: "16px 56px"
-            },
-            width: {
-              xs: "auto",
-              lg: "209px"
-            },
-            height: {
-              xs: "auto",
-              lg: "58px"
-            },
+            padding: { xs: "12px 24px", lg: "16px 56px" },
+            width: { xs: "auto", lg: "209px" },
+            height: { xs: "auto", lg: "58px" },
             borderRadius: "6px",
             textTransform: "none",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            "&:hover": {
-              background: "#0B45E6",
-            }
+            "&:hover": { background: "#0B45E6" }
           }}
         >
           Go Home
